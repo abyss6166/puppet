b957fe16f9bdc258e0b27b61cf319bf3e0108efe
Matt Horne <abyss616@yahoo.com>
2023-01-10 15:48:04 -0500

    fixes

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,10 +2,10 @@ class profile::base {
   # user {'admin':
   #   ensure => present
   # }
-  file { '/home/${user}/aliasbase':
+  file { '/home/mhorneus20/aliasbase':
     ensure => present,
     source => "puppet:///modules/master/aliasbase",
-    group => '${user}',
-    owner => '${user}',
+    group => 'mhorneus20',
+    owner => 'mhorneus20',
   }
 }
