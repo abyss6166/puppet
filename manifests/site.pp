687dae3f3fd67f571851105b865f97f26779dbd8
Matt Horne <abyss616@yahoo.com>
2023-01-10 15:33:54 -0500

    fixes

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,7 +8,7 @@ node 'ubuntuserver20' {
    }
   file { '/home/mhorneus20/aliasbase':
     ensure => present,
-    source => "puppet:///modules/aliasbase"
+    source => "puppet:///modules/master/aliasbase"
   }
 
 }
