f9a37009706ba2e0f7e42ae4837875c4ab3833ad
Matt Horne <abyss616@yahoo.com>
2023-01-10 16:11:48 -0500

    fixes

--- site/profile/manifests/master_server.pp
+++ site/profile/manifests/master_server.pp
@@ -1,12 +1,26 @@
+# class minecraft (
+# $serverurl='https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar',
+# $jdkurl='https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.rpm',
+# $install_dir='/opt/minecraft'
+# )
+
+#   file {"${install_dir}/minecraft_server.jar":
+#     ensure => file,
+#     source => $serverurl,
+#     before => Service['minecraft'],
+#   }
+
+
+# {
 class profile::master_server (
-  # $user='mhorneus20'
+  $user='mhorneus20',
   )
 {
-  file { '/home/mhorneus20/aliasus':
+  file { '/home/${user}/aliasus':
     ensure => present,
     source => "puppet:///modules/master/aliasus",
-    group => 'mhorneus20',
-    owner => 'mhorneus20',
+    group => $user,
+    owner => $user,
   }
 
 }
