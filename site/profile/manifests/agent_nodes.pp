fb97357f73207637d90a4ca95f1b92530fd9b793
Matt Horne <abyss616@yahoo.com>
2023-01-10 10:14:49 -0500

    fixes

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,7 +2,8 @@ class profile::agent_nodes {
   # include dockeragent
   # dockeragent::node {'web.puppet.vm':}
   node 'rocky9vm'
-  node 'rocky8vm'
-  node 'redhat9vm'
-  node 'redhat8vm'
 }
+
+  # node 'rocky8vm'
+  # node 'redhat9vm'
+  # node 'redhat8vm'
