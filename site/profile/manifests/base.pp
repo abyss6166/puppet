521a76a127a09e7a5a2b7d1924e095465c10968e
Matt Horne <abyss616@yahoo.com>
2023-01-10 11:36:43 -0500

    fixes

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -4,6 +4,6 @@ class profile::base {
   # }
   file { '/home/${user}/aliasbase':
     ensure => present,
-    source => "puppet:///files/aliasbase"
+    source => "puppet:///modules/files/aliasbase"
   }
 }
