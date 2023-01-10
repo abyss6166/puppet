6a9d5588369563fce8087adbca4b84e9764b10df
Matt Horne <abyss616@yahoo.com>
2023-01-10 15:27:20 -0500

    fixes

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,7 +2,7 @@ class profile::base {
   # user {'admin':
   #   ensure => present
   # }
-  file { 'temp.conf':
+  file { '/tmp/temp.conf':
     ensure => present,
     source => "puppet:///modules/temp.conf",
     path => '/tmp/temp.conf',
